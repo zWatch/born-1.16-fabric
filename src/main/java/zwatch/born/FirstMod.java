9ff5412624621332b905cebfe4f23056d41ad2cf
zWatch <z16c.nyj@gmail.com>
2021-03-24 18:03:14 +0800

    依然是第一个物品

--- src/main/java/zwatch/born/FirstMod.java
+++ src/main/java/zwatch/born/FirstMod.java
@@ -1,14 +1,37 @@
 package zwatch.born;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import zwatch.born.item.FirstItem;
 
 public class FirstMod implements ModInitializer {
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier("born", "general"),
+            () -> new ItemStack(Blocks.COBBLESTONE));
+
+    public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
+            new Identifier("born", "other"))
+            .icon(() -> new ItemStack(Items.BOWL))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(Blocks.BONE_BLOCK));
+                stacks.add(new ItemStack(Items.APPLE));
+                stacks.add(PotionUtil.setPotion(new ItemStack(Items.POTION), Potions.WATER));
+                stacks.add(ItemStack.EMPTY);
+                stacks.add(new ItemStack(Items.IRON_SHOVEL));
+            })
+            .build();
+
     public static final Item FirstItem  = new FirstItem(new FabricItemSettings().group(ItemGroup.MISC));
 
     @Override
--- src/main/java/zwatch/born/item/FirstItem.java
+++ src/main/java/zwatch/born/item/FirstItem.java
@@ -1,12 +1,17 @@
 package zwatch.born.item;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class FirstItem extends Item {
     public FirstItem(Settings settings) {
@@ -17,5 +22,36 @@ public class FirstItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        //tooltip.add(new TranslatableText("item.born.first_item.tooltip"));
+        int currentDay = 4;
+        int currentMonth = 7;
+        tooltip.add(new TranslatableText("item.born.first_item.tooltip", currentDay, currentMonth));
+        //Making \n work was far too difficult for Mojang,
+        // so in order to have a string with multiple lines you must split the translation key into multiple keys:
+        /*
+            {
+              "item.tutorial.fabric_item.tooltip_1": "Line 1 of my tooltip"
+              "item.tutorial.fabric_item.tooltip_2": "Line 2 of my tooltip"
+            }
+         */
+
+        /*
+        Object Type	Format	Example
+        Block	block.<namespace>.<path>	“block.tutorial.example_block”: “Example Block”
+        Item	item.<namespace>.<path>	“item.tutorial.my_item”: “My Item”
+        ItemGroup	itemGroup.<namespace>.<path>	“itemGroup.tutorial.my_group”: “My Group”
+        Fluid	fluid.<namespace>.<path>
+        SoundEvent	sound_event.<namespace>.<path>
+        StatusEffect	effect.<namespace>.<path>
+        Enchantment	enchantment.<namespace>.<path>
+        EntityType	entity.<namespace>.<path>
+        Biome	biome.<namespace>.<path>
+        Stat	stat.<namespace>.<path>
+        For types not in this list, see net.minecraft.util.registry.Registry.
+         */
     }
 }
